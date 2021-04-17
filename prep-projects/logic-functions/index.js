17c4979d646a43f87c006865dd24a88e97aaf9c1
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-17 22:25:36 +0300

    created index.html

---
+++ prep-projects/logic-functions/index.js
@@ -0,0 +1,17 @@
+/*var add = function (number1, number2) {
+    return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+    return number1 - number2;
+}
+var number1 = prompt("Enter a number");
+var number2 = prompt("Enter another number");
+alert(subtract(number1, number2));*/
+
+
+var weather = "rainy";
+weather === "sunny"  ? console.log("carry an umbrella"): console.log("go for a walk");
+
+var drink = "coffee";
+drink === "coffee" ? console.log("give me a cup of cofee"): console.log("a cup of tea please");
