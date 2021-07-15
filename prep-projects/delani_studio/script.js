693d965536600973e6f68832b9b1dd0cd4a4798d
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-15 20:20:41 +0300

    creatin django project

---
+++ prep-projects/delani_studio/script.js
@@ -0,0 +1,67 @@
+// $(document).ready(function() {
+//     $("p").click(function() {
+//       $(".walrus-showing").toggle();
+//       $(".walrus-hidden").toggle();
+//     });
+//   });
+$(document).ready(function() {
+    $("#design").click(function() {
+        $("#design").hide();
+        $(".design1").show();
+         });
+    });
+    $(document).ready(function() {
+        $(".design1").click(function() {
+            $("#design").show();
+            $(".design1").hide();
+             });
+        });
+    $(document).ready(function() {
+        $("#devop").click(function() {
+            $("#devop").hide();
+            $(".devop1").show();
+             });
+        });
+        $(document).ready(function() {
+            $(".devop1").click(function() {
+                $("#devop").show();
+                $(".devop1").hide();
+                 });
+            });
+        $(document).ready(function() {
+            $("#product").click(function() {
+                $(".product1").show();
+                $("#product").hide();
+                 });
+            });
+            $(document).ready(function() {
+                $(".product1").click(function() {
+                    $("#product").show();
+                    $(".product1").hide();
+                     });
+                });
+             $(document).ready(function() {
+                    $("#imgport").hover(function() {
+                        $(".text").show();
+                        
+                         });
+                    });
+
+                    mc-embedded-subscribe
+            $(document).ready(function() {
+                $("#mc-embeded-subscribe").click(function(){
+                    alert("Thank you for the message and for reaching out to us.")
+                }
+            }
+            // p {
+            //     cursor: pointer;
+            //     color: #0088cc;
+            //     }
+                
+            //     p:hover {
+            //     text-decoration: underline;
+            //     }
+
+            // .portfolio .row img:hover {
+            //     background: white;
+            // }
