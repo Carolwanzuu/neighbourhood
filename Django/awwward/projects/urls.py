d52c8b7957e5ebf7c067dba716b59b19ca13fe10
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-15 23:37:15 +0300

    displayed index template

--- Django/awwward/projects/urls.py
+++ Django/awwward/projects/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
 
-# urlpatterns = [
-#     path('',views.welcome,name = 'welcome'),
-# ]
+urlpatterns = [
+    path('',views.welcome,name = 'welcome'),
+    
+]
