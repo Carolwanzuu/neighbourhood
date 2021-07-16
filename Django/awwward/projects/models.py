b945d100ed54dca3898ea83ea6aa4697d90e3ec2
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-16 07:34:38 +0300

    creating a profile model

--- Django/awwward/projects/models.py
+++ Django/awwward/projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic=models.ImageField(blank=True, default='default.jpg')
+    bio=models.TextField(max_length=255, default='My Bio',blank=True)
+    name=models.CharField(max_length=50,blank=True)
+
+
+
