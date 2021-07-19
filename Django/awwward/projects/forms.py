f56610d46e0d587be1b5eadcd8b04aed5c1e2394
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-19 04:15:32 +0300

    adding forms

--- Django/awwward/projects/forms.py
+++ Django/awwward/projects/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms.fields import ImageField
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, Projects,Review,RATE_CHOICES
 
 
@@ -14,4 +16,15 @@ class projectForm(forms.ModelForm):
     class Meta:
         model = Projects
         fields = ['projectPhoto', 'title', 'projectUrl', 'description', 'technologies']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email'] 
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['Bio', 'photo']
     
--- Django/awwward/projects/views.py
+++ Django/awwward/projects/views.py
@@ -10,10 +10,25 @@ def register(request):
     return render(request, 'users/register.html')
 
 def Profile(request):
-    
     return render(request, 'profile.html')
 
 def edit_Profile(request):
+    user= request.user
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.id)
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'editprofile.html', params)
     return render(request,'editProfile.html')
 
 def project(request):
