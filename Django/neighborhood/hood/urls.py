be650f27d417939d78fec1e5284b79e7c05c0ba9
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-26 18:35:31 +0300

    adding single-hood template

--- Django/neighborhood/hood/urls.py
+++ Django/neighborhood/hood/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('new_hood/', views.new_hood, name='new_hood'),
     path('join_hood/<id>', views.join_hood, name='join-hood'),
     path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
 ]
